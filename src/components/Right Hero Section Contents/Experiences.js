0c0c70b79f9fc37cba49c150d9d194b8f9aebb8e
hitenvats16 <hitenvats16@gmail.com>
2022-08-24 11:51:10 +0530

    updated the section

--- src/components/Right Hero Section Contents/Experiences.js
+++ src/components/Right Hero Section Contents/Experiences.js
@@ -25,11 +25,12 @@ const Experiences = () => {
     <div className="w-full h-[92%] xl:h-full p-5 ">
       <div className="w-full xl:p-10 h-full sm:h-[92%] h-max-xl overflow-y-auto scrollbar">
         <ol className="relative border-l border-non-active">
+          <Card title="Teaching Assistant, Coding Ninjas" time="August 2022 - Present" discription="Responsible for debugging student’s code and explain the concepts & intuition behind the approach towards problem."/>
           <Card title="HACKOVERFLOW4.0 HACKATHON, Hackoverflow society, CU" time="April 2022 (5 Days long)" discription="Created a Geo Tracking solution to keep the track of the location
               and behaviour of the bus like speed, remaining time etc for
               schools and parents. Got 4th rank, Event had recieved 5000+
               registrations."/>
-          <Card title="Design Heist, BVPIEEE, BVCOE" time="February 2022 (12 Days long)" discription="Created a Design for a hypothetical OTT Plateform and covert that into code using ReactJS and Talwind CSS. We were among the finalists in the Event."/>
+          <Card title="Design Heist, BVPIEEE, BVCOE" time="February 2022 (12 Days long)" discription="Created a Design for a hypothetical OTT Plateform and covert that into code using ReactJS and Talwind CSS. We were among the finalists in the Event." />
           <Card title="Hack The League, Online" time="January 2022 (1 Month long)" discription="Created a solution for exchanging and lending books for college
               and school students. Users was able to connect with each other
               over email."/>
